8b073fe83f872ea8676b6e535939e326a4a98c3f
pranhaas <76516218+pranhaas@users.noreply.github.com>
2022-01-26 11:35:34 +0000

    26-1-22

--- passwordchecker.py
+++ passwordchecker.py
@@ -4,18 +4,30 @@ import re
 def passcheck():
     score = 0
     passw = input("What is your password?\n")
+    allowed = list("qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM!$%&*()_-+=1234567890")
+    not_allowed_bad_human_stuff = ["qwe","rty","uio","QWE","RTY","UIO","asd","fgh", "jkl", "ASD", "FGH", "JKL", "zxc", "vbn", "ZXC", "VBN"]
     if (len(passw)<8) or (len(passw)>24):
         print("Error!\npassword needs to be between 8 and 24")
         passcheck()
-    letters = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
-    numbers = ["1","2","3","4","5","6","7","8","9","0"]
-    otherones = ["!","$","%","&","*","(",")","-","_","=","+"]
+    else:
+        for i in allowed:
+            score = score + 1
+            print(score)
+    print(allowed)
+    
+    if socre > 15:
+        print("Your password is strong!")
+    elif score <15 and >7:
+        print("Password is moderate in strenth")
+    else: 
+        print("PASSWORD IS VERY WEEK!!")
+
 
     menu()
 def genpass():
 
     pass1 = ""
-    words = list("qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM!$%&*()_-+=")
+    words = list("qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM!$%&*()_-+=1234567890")
     ammount = 0
     while ammount < 24:
         part = random.choice(words)
