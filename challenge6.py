25e6b37f464a0c4b9545f2761cd6f77eb347dfa4
pranhaas <76516218+pranhaas@users.noreply.github.com>
2023-02-28 09:39:01 +0000

    added round

--- challenge6.py
+++ challenge6.py
@@ -1,11 +1,10 @@
 import time as t
 a = t.time()
-input("Press the enter key when you think the correct time has passed")
+input("Press the enter key when you think 10 seconds has passed")
 b = t.time()
+a = round(a)
+b = round(b)
 total = b-a
-print(f"Your time was {total}.\n You were {10-total} far away")
+print(f"Your time was {total}s.\n You were {10-total}s far away")
 
 input()
-
-
-
