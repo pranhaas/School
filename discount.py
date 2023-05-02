c0a89dfe8ccdf1bb9ec55b57b5b023afb3ad7184
pranhaas <76516218+pranhaas@users.noreply.github.com>
2023-05-02 09:38:04 +0100

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,8 +1,18 @@
-name = input("What is your name?")
-budegt = float(input("What is your budet"))
-age = int(input("What is your age?"))
+name = input("What is your name? \n")
+budegt = float(input("What is your budet \n"))
+age = int(input("What is your age? \n"))
+dept = input("What department would you like to shop in?\n")
 if age >= 60:
     budegt = budegt * 0.75
 elif age <= 18:
     budegt = budegt * 0.90
+elif dept == "homewear":
+    budegt = budegt * 0.9
+elif dept == "womenswear":
+    budegt = budegt * 0.95
+elif dept == "menswear":
+    budegt = budegt * 0.85
+elif dept == "shoes" and age == 30:
+    budegt = budegt * 0.5
 print(budegt)
+
