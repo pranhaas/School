d4b0ba4ba232099e0481207264466c8974beb68f
pranhaas <76516218+pranhaas@users.noreply.github.com>
2023-05-02 09:58:05 +0100

    Create nick.py

---
+++ nick.py
@@ -0,0 +1,6 @@
+import random as r
+number = r.randint(0,10)
+names = ["dave","mr hyde","mr seek","daisy renton","eva smith","kayan","william","laptop","pc","mr birling"]
+fname = input("What is your first name?")
+lname = input("What is your second name?")
+print(f"{fname} {names[number]} {lname}")
